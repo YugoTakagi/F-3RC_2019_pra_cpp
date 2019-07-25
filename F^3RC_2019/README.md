aab98db5b6f00b16b02a799c7e0b469f44849862
高城友豪 <a217048@ns.kogakuin.ac.jp>
2019-07-25 16:21:27 +0900

    making standerd input/output
    making virtual hlow.
    making applications.

--- F^3RC_2019/README.md
+++
@@ -1,1 +0,0 @@
-# F-3RC_2019_to_practice_cpp
--- F^3RC_2019/main.cpp
+++ F^3RC_2019/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include "LowlayerHandel.hpp"
+#include "curiosity.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    LowlayerHandelTypedef hlow;
+    Curiosity cy(&hlow, 20*10^-3);
+    Datas State;
+    Datas Reference;
+    State.x=0.0;
+    State.y=0.0;
+    State.theta=0.0;
+    Reference.x=1.0;
+    Reference.y=1.0;
+    Reference.theta=1.0;
+    
+    
+    cy.using_model(State, Reference);
     return 0;
 }
