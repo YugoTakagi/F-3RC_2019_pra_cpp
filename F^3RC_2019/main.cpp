27a9ee0839ab79371dc6263fa736f9fc339d2e86
高城友豪 <a217048@ns.kogakuin.ac.jp>
2019-07-25 16:09:05 +0900

    Initial Commit

---
+++ F^3RC_2019/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  F^3RC_2019
+//
+//  Created by 高城友豪 on 2019/07/25.
+//  Copyright © 2019 高城友豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
